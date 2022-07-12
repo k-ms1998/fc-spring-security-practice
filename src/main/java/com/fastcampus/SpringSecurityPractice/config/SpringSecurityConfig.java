b4432a32e935c332246b9c0ba13296a82fd8a02a
Min Seop Kim <k_ms1998@naver.com>
2022-07-12 16:38:01 +0900

    Minor Edits

--- src/main/java/com/fastcampus/SpringSecurityPractice/config/SpringSecurityConfig.java
+++ src/main/java/com/fastcampus/SpringSecurityPractice/config/SpringSecurityConfig.java
@@ -1,6 +1,8 @@
 package com.fastcampus.SpringSecurityPractice.config;
 
+import com.fastcampus.SpringSecurityPractice.domain.admin.Admin;
 import com.fastcampus.SpringSecurityPractice.domain.user.User;
+import com.fastcampus.SpringSecurityPractice.service.AdminService;
 import com.fastcampus.SpringSecurityPractice.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
@@ -21,15 +23,19 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final UserService userService;
+    private final AdminService adminService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         // basic authentication
         http.httpBasic().disable(); // basic authentication filter 비활성화
+
         // csrf
         http.csrf();
+
         // remember-me
         http.rememberMe();
+
         // authorization
         http.authorizeRequests()
                 // /와 /home은 모두에게 허용
@@ -37,13 +43,14 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
                 // hello 페이지는 USER 롤을 가진 유저에게만 허용
                 .antMatchers("/note").hasRole("USER")
                 .antMatchers("/admin").hasRole("ADMIN")
+                .antMatchers(HttpMethod.GET, "/notice").permitAll()
                 .antMatchers(HttpMethod.POST, "/notice").hasRole("ADMIN")
                 .antMatchers(HttpMethod.DELETE, "/notice").hasRole("ADMIN")
                 .anyRequest().authenticated();
 
         // login
         http.formLogin()
-                .loginPage("/login")
+                .loginPage("/my/login") // 로그인 페이지에서 FORM 데이터를 보낼 URL; th:action="@{/my/login}"
                 .defaultSuccessUrl("/")
                 .permitAll(); // 모두 허용
 
@@ -69,11 +76,21 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public UserDetailsService userDetailsService() {
         return username -> {
-            User user = userService.findByUsername(username);
-            if (user == null) {
-                throw new UsernameNotFoundException(username);
+            if (username.contains("admin")) {
+                Admin admin = adminService.findByAdminName(username);
+                if (admin == null) {
+                    throw new UsernameNotFoundException(username);
+                }
+                return admin;
+
+            } else {
+                User user = userService.findByUsername(username);
+                if (user == null) {
+                    throw new UsernameNotFoundException(username);
+                }
+                return user;
             }
-            return user;
+
         };
     }
 }
